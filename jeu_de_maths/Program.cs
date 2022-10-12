ea279028bab0c99d9bf451e2b2cdf175d82c331b
Caroline Owen <carolinaowen95@gmail.com>
2022-10-12 17:59:35 +0200

    fini

--- jeu_de_maths/Program.cs
+++ jeu_de_maths/Program.cs
@@ -1,42 +1,108 @@
 ﻿// See https://aka.ms/new-console-template for more information
-static bool PoserQuestion(int min, int max)
-    {
-    int reponse_nombre = 0;
-    Random rand = new Random();
-    while(true)//boucler de manière infini
-    {
-        int a = rand.Next(min, max+1);
-        int b = rand.Next(min, max+1);
-        Console.WriteLine($"quel est le résultat de {a} + {b} ?");
-        string reponse = Console.ReadLine();
 
-        try
+namespace jeu_de_maths
+{
+    class Program
+    {
+        enum e_Operateur
         {
-            reponse_nombre = int.Parse(reponse);
-            if (reponse_nombre == a + b)
+            ADDITION = 1,
+            MULTIPLICATION = 2,
+            SOUSTRACTION = 3
+        }
+
+
+        static bool PoserQuestion(int min, int max)
+        {
+            int reponse_nombre = 0;
+            Random rand = new Random();
+            while (true)//boucler de manière infini
             {
-                return true;
+                int a = rand.Next(min, max + 1);
+                int b = rand.Next(min, max + 1);
+                e_Operateur o = (e_Operateur)rand.Next(1, 4);
+                int resultatAttendu;
+
+                if (o == e_Operateur.ADDITION)
+                {
+                    Console.WriteLine($"quel est le résultat de {a} + {b} ?");
+                    resultatAttendu = a + b;
+                }
+                else if(o == e_Operateur.MULTIPLICATION)
+                {
+                    Console.WriteLine($"quel est le résultat de {a} * {b} ?");
+                    resultatAttendu = b * a;
+                }
+                else
+                {
+                    Console.WriteLine($"quel est le résultat de {a} - {b} ?");
+                    resultatAttendu = a - b;
+                }
+
+                string reponse = Console.ReadLine();
+
+                try
+                {
+                    reponse_nombre = int.Parse(reponse);
+                    if (reponse_nombre == resultatAttendu)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                catch
+                {
+                    Console.WriteLine("le format de réponse n'est pas valide");
+
+                }
             }
-            return false;
+
         }
-        catch
+
+        static void Main(string[] args)
         {
-            Console.WriteLine("le format de réponse n'est pas valide");
-           
+            int min = 1;
+            int max = 10;
+            int nbQuestions = 4;
+            int points = 0;
+
+            for (int i = 0; i < nbQuestions; i++)
+            {
+                Console.WriteLine($"Question n° {i + 1} / {nbQuestions}");
+                bool bonneReponse = PoserQuestion(min, max);
+                if (bonneReponse)
+                {
+                    Console.WriteLine("Bonne réponse");
+                    points++;
+                }
+                else
+                {
+                    Console.WriteLine("mauvaise réponse");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine($"Vous avez obtenu {points} / {nbQuestions} points");
+            int average = nbQuestions / 2;
+
+            if (points == nbQuestions)
+            {
+                Console.WriteLine("Excelent");
+            }
+            else if (points == 0)
+            {
+                Console.WriteLine("Révisez vos Maths");
+            }
+            else if (points >= average)
+            {
+                Console.WriteLine("pas mal");
+            }
+            else
+            {
+                Console.WriteLine("peu mieux faire");
+            }
         }
-    }
-   
-    }
-
-int min = 1;
-int max = 10;
-
-bool bonneReponse = PoserQuestion(min, max);
-if (bonneReponse)
-{
-    Console.WriteLine("Bonne réponse");
+        
 }
-else
-{
-    Console.WriteLine("mauvaise réponse");
 }
+
