49fe0e3657726d551814411b88a9186e66ec05b8
Caroline Owen <carolinaowen95@gmail.com>
2022-10-12 16:32:08 +0200

    Ajoutez des fichiers projet.

---
+++ jeu_de_maths/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+static bool PoserQuestion(int min, int max)
+    {
+    int reponse_nombre = 0;
+    Random rand = new Random();
+    while(true)//boucler de manière infini
+    {
+        int a = rand.Next(min, max+1);
+        int b = rand.Next(min, max+1);
+        Console.WriteLine($"quel est le résultat de {a} + {b} ?");
+        string reponse = Console.ReadLine();
+
+        try
+        {
+            reponse_nombre = int.Parse(reponse);
+            if (reponse_nombre == a + b)
+            {
+                return true;
+            }
+            return false;
+        }
+        catch
+        {
+            Console.WriteLine("le format de réponse n'est pas valide");
+           
+        }
+    }
+   
+    }
+
+int min = 1;
+int max = 10;
+
+bool bonneReponse = PoserQuestion(min, max);
+if (bonneReponse)
+{
+    Console.WriteLine("Bonne réponse");
+}
+else
+{
+    Console.WriteLine("mauvaise réponse");
+}
